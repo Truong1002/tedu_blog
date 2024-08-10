19663aca9017ea07dc82d6b8e732b2683977f464
Truong1002 <truongphamxuan724@gmail.com>
2024-08-11 00:01:31 +0700

    Complete PostAtvLog.cs

--- src/TeduBlog.Core/Domain/Content/PostActivityLog.cs
+++ src/TeduBlog.Core/Domain/Content/PostActivityLog.cs
@@ -1,12 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace TeduBlog.Core.Domain.Content
 {
+    [Table("PostActivityLogs")]
     public class PostActivityLog
     {
+        [Key]
+        public Guid Id { get; set; }
+
+        public Guid PostId { get; set; }
+
+        public PostStatus FromStatus { get; set; }
+        public PostStatus ToStatus { get; set; }
+        public DateTime DateCreated { get; set; }
+
+        [MaxLength(500)]
+        public string? Note { get; set; }
+
+        public Guid UserId { get; set; }
     }
 }
